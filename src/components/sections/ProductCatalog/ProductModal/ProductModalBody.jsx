a63dcf801595c9d583e44897f72ea35197c87b01
MYA Project <urusanyasif@gmail.com>
2026-06-28 14:30:49 +0700

    update no hp

--- src/components/sections/ProductCatalog/ProductModal/ProductModalBody.jsx
+++ src/components/sections/ProductCatalog/ProductModal/ProductModalBody.jsx
@@ -19,7 +19,7 @@ export default function ProductModalBody({ product, selectedVariant, onVariantCh
         product={product}
         selectedVariant={selectedVariant}
         onVariantChange={onVariantChange}
-        phone={"08973172170"}
+        phone={"628973172170"}
       />
     </div>
   );
